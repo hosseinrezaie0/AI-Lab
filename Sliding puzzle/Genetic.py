ff3fb4f06116ec0c9badaedf1ccbea70f7532d22
hosseinrezaie0 <hossein.rezaie81@gmail.com>
2024-07-20 21:38:50 +0330

    initial population function added

--- Sliding puzzle/Genetic.py
+++ Sliding puzzle/Genetic.py
@@ -1,1 +1,17 @@
+import random
 
+
+# Setting Parameters
+N = 3           # N*N puzzle
+PS = 100        # Population Size
+MR = 0.8        # Mutation Rate
+
+# Create initial population
+def init_population(N, PS):
+    population = []
+    for i in range(PS):
+        member = []
+        for i in range(1,N*N):
+            member = random.sample(range(1, N*N), N*N-1)
+        population.append(member + [None])              # None is for fitness evaluation
+    return population
