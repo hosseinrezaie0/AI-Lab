071408bfe0208215823a5a4352f4a4f5571d0b4b
hosseinrezaie0 <hossein.rezaie81@gmail.com>
2024-07-22 13:42:01 +0330

    cross over function added

--- Sliding puzzle/Genetic.py
+++ Sliding puzzle/Genetic.py
@@ -3,7 +3,7 @@ import random
 
 # Setting Parameters
 N = 3           # N*N puzzle
-PS = 100        # Population Size
+PS = 10        # Population Size
 MR = 0.8        # Mutation Rate
 
 # Create initial population
@@ -15,3 +15,13 @@ def init_population(N, PS):
             member = random.sample(range(1, N*N), N*N-1)
         population.append(member + [None])              # None is for fitness evaluation
     return population
+
+
+# Cross over
+def cross_over(population, N, PS):
+    for i in range(0,PS,2):
+        child1 = population[i][:N*N//2] + population[i+1][N*N//2:]
+        child2 = population[i+1][:N*N//2] + population[i][N*N//2:]
+        population.append(child1)
+        population.append(child2)
+    return population 
